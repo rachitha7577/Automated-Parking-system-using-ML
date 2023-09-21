b524c9610e1cac351545eecc43f8f0a07836a5b5
rachitha7577 <145669199+rachitha7577@users.noreply.github.com>
2023-09-21 16:47:40 +0530

    Add files via upload

---
+++ carparking.py
@@ -0,0 +1,49 @@
+#Author: A. N. M. Sajedul Alam
+#Date: 23/01/2023
+import cv2
+import pickle
+
+
+cap=cv2.VideoCapture("carpark.mp4")
+#success,frame=cap.read()
+#frame=cv2.resize(frame,(640,480))
+#roi=cv2.selectROI(frame)
+#196,373,225,400
+#10, 5, 6, 4
+try:
+    with open("cparkyt",'rb')as f:
+            points=pickle.load(f)
+except:
+    points=[]
+def drawing(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        points.append((x,y))
+        with open("cparkyt",'wb')as f:
+            pickle.dump(points,f)
+    if event==cv2.EVENT_RBUTTONDOWN:
+       for i,pts in enumerate(points):
+           x1,y1=pts
+           if x1<x+w and y1<y+h:
+               points.pop(i)
+            
+        
+
+#w,h=29,27
+w,h=20,20
+while True:
+    success,frame=cap.read()
+    if success==False:
+        break
+
+    frame=cv2.resize(frame,(640,480))
+    for pts in points:
+        cv2.rectangle(frame,pts,(pts[0]+w,pts[1]+h),(0,0,255),2)
+    
+    
+  
+    cv2.imshow("Frame", frame)
+    cv2.setMouseCallback("Frame",drawing)
+    if cv2.waitKey(32) & 0xFF == 27:
+        break
+cap.release()    
+cv2.destroyAllWindows()
